865876846f77ce8b4520a038a3991e383ced11a5
wonkcolm <suwardi.helpdesk@gmail.com>
2022-02-02 23:38:12 +0700

    db_connected

---
+++ adi/connect_db_client.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password=''
+)
+
+if db.is_connected():
+    print('berhasil terhubung kedatabase')
+else:
+    print('tidak dapat terhubung')
+
+
+
+
---
+++ adi/main.py
@@ -0,0 +1,12 @@
+#print('test');
+#lesson 2
+nama ='nurul huda'
+usia = 24
+sudah_menikah = 'true'
+
+#print(nama)
+#print(usia)
+#print(sudah_menikah)
+
+#print ('nama saya',nama,'usia', usia,'dan saya', sudah_menikah)
+
